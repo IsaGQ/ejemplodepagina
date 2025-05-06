1adfd80a9680109404cb357d7cbd9be3afd0d11d
IsaGQ <isagomezquiroz@gmail.com>
2025-05-06 18:40:19 -0500

    first commit

---
+++ src/const/index.js
@@ -0,0 +1,6 @@
+import PROJECTS from "./projects";
+import RESOURCES from "./resources";
+import PERMISSIONS from "./permissions";
+
+export {PROJECTS, RESOURCES, PERMISSIONS}
+
---
+++ src/screens/Auth/SignIn/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Layout from "../components/LoginFormSmart";
+import LoginFormSmart from "../containers/LoginFormSmart";
+
+export default function SignInScreen() {
+    return (
+        <Layout>
+            <LoginFormSmart />
+        </Layout>
+    );
+}
